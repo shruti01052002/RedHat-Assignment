8e99ed79f456282abbbc239a23980c88555c7003
Shruti Yadav <shrutiyadav26072002@gmail.com>
2022-10-13 14:52:50 +0530

    Get Order Api Added

--- pizza-house/main.py
+++ pizza-house/main.py
@@ -1,19 +1,55 @@
-from flask import Flask, render_template, request, redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
+import imp
+from msilib.schema import tables
+# from typing_extensions import Required
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_restful import Resource, Api, reqparse, abort, fields
+from flask_mongoengine import MongoEngine
 from datetime import datetime
-# from flask_mail import Mail
+from mongoengine import *
 from flask_pymongo import PyMongo
 import json
 import os
-main = Flask(__name__)
-main.config['MONGO_URI'] = 'mongodb+srv://Shruti:2019b121002@pizza-house.sxrdcrv.mongodb.net/test'
-pizza_house = PyMongo(main)
+app = Flask(__name__)
+main = Api(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'pizza_house',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+class Order(db.Document):
+    # id = db.IntField(primary_key=True)
+    order = db.ListField(required=True)
+    
+    def __repr__(self):
+        return 'Order '+str(self.order)
+orders = {"order": ["Pizza1", "Pizza2"]}
 
 
-@main.route('/welcome')
+order_post_args = reqparse.RequestParser()
+order_post_args.add_argument("order", type=list, help="order is required", required=True)
+    
+
+# print(order_post_args["order"])
+@app.route('/welcome')
 def base():
     return '<h1>Welcome to Pizza House</h1>'
 
+@app.route('/order')
+def orderdetails():
+    # request_data = request.get_json()
+    args = {"order":["MediumPizza", "LargePizza"]}
+    order = Order(order=args["order"]).save()
+    return jsonify(order)
+li = []
+@app.route('/getorders')
+def getorders():
+    for order in Order.objects():
+        li.append(order)
+    return jsonify(li)
 
+# @app.route('/getorders')
 if __name__ == "__main__":
-    main.run(debug=True)
+    app.run(debug=True)
